b6bb7b21e2aaaca975bd04a7fdf2fdacbf225786
szabelnikov <szabelnikov@star.global>
2019-09-26 15:43:13 +0300

    Add wallpaper to android

--- wallpaper_changer/android/src/main/kotlin/com/regin/wallpaper_changer/WallpaperChangerPlugin.kt
+++ wallpaper_changer/android/src/main/kotlin/com/regin/wallpaper_changer/WallpaperChangerPlugin.kt
@@ -1,23 +1,21 @@
 package com.regin.wallpaper_changer
 
 import android.app.Activity
-import android.app.Application
-import android.graphics.Bitmap
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 import android.app.WallpaperManager
-import android.net.Uri
-import java.io.IOException
-import com.sun.tools.corba.se.idl.Util.getAbsolutePath
 import android.graphics.BitmapFactory
-
 
 class WallpaperChangerPlugin(private val activity: Activity) : MethodCallHandler {
 
     companion object {
+
+        const val WALLPAPER_PATH_KEY: String = "wallpaperPathKey"
+        const val WALLPAPER_SCREEN_KEY: String = "wallpaperScreenKey"
+
         @JvmStatic
         fun registerWith(registrar: Registrar) {
             val channel = MethodChannel(registrar.messenger(), "wallpaper_changer")
@@ -27,20 +25,21 @@ class WallpaperChangerPlugin(private val activity: Activity) : MethodCallHandler
 
     override fun onMethodCall(call: MethodCall, result: Result) {
         if (call.method == "setWallpaper") {
+            setWallpaper(call.argument(WALLPAPER_PATH_KEY)!!, call.argument(WALLPAPER_SCREEN_KEY)!!)
 
-            result.success()
+            result.success("")
         } else {
             result.notImplemented()
         }
     }
 
-    fun setWallpaper(uri: Uri) {
+    private fun setWallpaper(filePath: String, screenIndex: Int) {
         val wallpaperManager = WallpaperManager.getInstance(activity)
         try {
-            val bitmap = BitmapFactory.decodeFile(uri.path)
+            val bitmap = BitmapFactory.decodeFile(filePath)
 
             wallpaperManager.setBitmap(bitmap)
-        } catch (ex: IOException) {
+        } catch (ex: Throwable) {
             ex.printStackTrace()
         }
 
--- wallpaper_changer/lib/wallpaper_changer.dart
+++ wallpaper_changer/lib/wallpaper_changer.dart
@@ -1,13 +1,39 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
 
 class WallpaperChanger {
+  static const String tempFileName = 'temp.png';
+
+  static const String setWallpaperMethod = 'setWallpaper';
+  static const String wallpaperPathKey = 'wallpaperPathKey';
+  static const String wallpaperScreenKey = 'wallpaperScreenKey';
+
   static const MethodChannel _channel =
       const MethodChannel('wallpaper_changer');
 
   static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+    final String version = await _channel.invokeMethod('setWallpaper');
     return version;
   }
+
+  static Future<void> setWallpaper(String url, Screen screen) async {
+    final folder = await getTemporaryDirectory();
+
+    var fileSave = new File('${folder.path}/$tempFileName');
+
+    final response = await http.get(url);
+
+    await fileSave.writeAsBytes(response.bodyBytes);
+
+    _channel.invokeMethod(setWallpaperMethod, <String, dynamic>{
+      wallpaperPathKey: fileSave.path,
+      wallpaperScreenKey: screen.index,
+    });
+  }
 }
+
+enum Screen { Home, Lock }
